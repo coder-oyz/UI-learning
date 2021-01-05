dfcdfd18c5327149f54b18fa654ebaa7b074cadc
coder-oyz <1583807155@qq.com>
2021-01-05 10:23:06 +0800

    TextView，button ，editText，imageView，progressBar，AlertDialog

--- app/src/main/java/com/oyz/uiwidgettest/MainActivity.kt
+++ app/src/main/java/com/oyz/uiwidgettest/MainActivity.kt
@@ -2,10 +2,61 @@ package com.oyz.uiwidgettest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //设置按钮的点击事件
+       /* button.setOnClickListener {
+            // 在此处添加逻辑
+        }*/
+        //也可以使MainActivity实现View.OnClickListener,重写onClick方法
+        button.setOnClickListener (this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.button -> {
+                //使用Toast显示文本框中的文本
+                /*val inputText = editText.text.toString()
+                Log.d("MainActivity-------",inputText)
+                Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show()*/
+
+                //动态改变图片
+                //imageView.setImageResource(R.drawable.img_2)
+
+                //使进度条 如果可见就隐藏，不可见就显示
+                /*progressBar.visibility= if(progressBar.visibility == View.VISIBLE) {
+                    View.GONE
+                }else{
+                    View.VISIBLE
+                }*/
+
+                //动态改变进度条进度
+                //progressBar.progress = progressBar.progress + 10
+
+                //对话框
+                AlertDialog.Builder(this).apply {
+                    setTitle("This is Dialog")
+                    setMessage("Something important.")
+                    setCancelable(false)  //可否使用back键关闭对话框
+                    setPositiveButton("OK") { dialog, which ->
+                    }
+                    setNegativeButton("Cancel") { dialog, which ->
+                    }
+                    show()
+                }
+
+
+
+            }
+        }
     }
 }
