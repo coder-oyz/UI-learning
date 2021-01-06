576bd03c4afd3db3ddab093e50b82482805e5123
coder-oyz <1583807155@qq.com>
2021-01-06 10:10:45 +0800

    自定义ListView完成

---
+++ app/src/main/java/com/oyz/listviewtest/Fruit.kt
@@ -0,0 +1,3 @@
+package com.oyz.listviewtest
+
+class Fruit(val name:String, val imageId: Int)
--- app/src/main/java/com/oyz/listviewtest/MainActivity.kt
+++ app/src/main/java/com/oyz/listviewtest/MainActivity.kt
@@ -2,10 +2,52 @@ package com.oyz.listviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //listView简单用法
+    //初始化数据
+    /*private val data = listOf("Apple", "Banana", "Orange", "Watermelon",
+        "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
+        "Pineapple", "Strawberry", "Cherry", "Mango")
+    */
+
+    private val fruitList = ArrayList<Fruit>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //使用适配器将集合中的数据传递给listView
+        /*val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data)
+        listView.adapter = adapter*/
+
+
+        initFruits() // 初始化水果数据
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
+        listView.adapter = adapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+
+        }
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val fruit = fruitList[position]
+            Toast.makeText(this, fruit.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+    //repeat(2) 表示添加两遍
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
     }
 }
