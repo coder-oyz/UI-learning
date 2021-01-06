2dda985ab3e2bcf977b303d1a93ced69482e3893
coder-oyz <1583807155@qq.com>
2021-01-06 09:43:58 +0800

    自定义title，使用include插入，同时隐藏自带的

--- app/src/main/java/com/oyz/uicustomviews/MainActivity.kt
+++ app/src/main/java/com/oyz/uicustomviews/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //隐藏系统自带的标题栏
+        supportActionBar?.hide()
+
     }
 }
